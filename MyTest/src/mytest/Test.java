bdc98bbe5f7e94da8ea6b6dafc26bc1c592e03a6
wchen-talend <wchen@talend.com>
2014-06-25 11:33:59 +0800

    ggg ggg fff eee
    Conflicts:
    	MyTest/src/mytest/Test.java

--- MyTest/src/mytest/Test.java
+++ MyTest/src/mytest/Test.java
@@ -5,6 +5,11 @@ public class Test {
 	public Test(){
 		String ccc="ccc";
 		String ddd="ddd";
+		String dddd="ddd";
+		String eee="eee";
+		String fff="fff";
+		String ggg="ggg";
+		String hhh="hhh";
 	}
 	
 	public void newFunction(){
