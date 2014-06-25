72554baa300364f1eceb017b5c9e3d1d8c99d67c
wchen-talend <wchen@talend.com>
2014-06-25 11:33:59 +0800

    ggg ggg fff eee

--- MyTest/src/mytest/Test.java
+++ MyTest/src/mytest/Test.java
@@ -6,6 +6,10 @@ public class Test {
 		String ccc="ccc";
 		String ddd="ddd";
 		String dddd="ddd";
+		String eee="eee";
+		String fff="fff";
+		String ggg="ggg";
+		String hhh="hhh";
 	}
 	
 	public void newFunction(){
